d6d3ed9e04d235f9ce9002aa6fd9bd0a90c2c83b
mistadikay <koltsov.d@gmail.com>
2015-08-28 16:14:03 +0700

    :hatching_chick:

---
+++ test/karma.common.js
@@ -0,0 +1,51 @@
+import path from 'path';
+
+export default {
+    colors: true,
+    files: [
+        'lib/*.js'
+    ],
+    preprocessors: {
+        'lib/*.js': 'webpack'
+    },
+    frameworks: [ 'mocha' ],
+    webpack: {
+        cache: true,
+        resolve: {
+            alias: {
+                '~': path.resolve('./')
+            }
+        },
+        module: {
+            preLoaders: [
+                {
+                    test: /\.js$/,
+                    exclude: [
+                        path.resolve('lib/'),
+                        path.resolve('node_modules/')
+                    ],
+                    loader: 'babel'
+                },
+                {
+                    test: /\.js$/,
+                    include: path.resolve('lib/'),
+                    loader: 'isparta'
+                }
+            ]
+        }
+    },
+    webpackMiddleware: {
+        noInfo: true,
+        quiet: true
+    },
+    coverageReporter: {
+        dir: '../coverage',
+        reporters: [
+            { type: 'lcovonly', subdir: '.' }
+        ]
+    },
+    browserNoActivityTimeout: 60 * 1000, // default 10 * 1000
+    browserDisconnectTimeout: 10 * 1000, // default 2 * 1000
+    browserDisconnectTolerance: 2, // default 0
+    captureTimeout: 2 * 60 * 1000 // default 1 * 60 * 1000
+};
---
+++ test/lib/index.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import chaiBEM from '~/lib';
+
+describe('index', () => {
+    it('exists', () => {
+        expect(chaiBEM).to.exist;
+    });
+});
