1961b040ec1d633f05b06825477d69711e3d696c
mistadikay <koltsov.d@gmail.com>
2015-08-29 19:18:51 +0700

    :heavy_plus_sign: add basic `elem` assertion

--- test/lib/index.js
+++ test/lib/index.js
@@ -13,12 +13,32 @@ describe('index', () => {
             expect('input').to.be.a.block('input');
         });
 
-        it('with lots of spaces', () => {
-            expect('  input   input_focused      ').to.be.a.block('input');
+        it('not first', () => {
+            expect('input_focused input').to.be.a.block('input');
         });
 
-        it('not first', () => {
-            expect('  input_focused   input   ').to.be.a.block('input');
+        it.skip('TODO throw error tests', () => {
+
+        });
+    });
+
+    describe('elem', () => {
+        it('simple', () => {
+            expect('input input__control').to.be.an.elem({
+                block: 'input',
+                elem: 'control'
+            });
+        });
+
+        it('within a mix', () => {
+            expect('input input__control sidebar__input').to.be.an.elem({
+                block: 'sidebar',
+                elem: 'input'
+            });
+        });
+
+        it.skip('TODO throw error tests', () => {
+
         });
     });
 });
