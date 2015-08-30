88f7c995d40abf15a6b0994a342bedccb8998330
mistadikay <koltsov.d@gmail.com>
2015-08-30 21:32:39 +0700

    :space_invader: tests for negative assertions

--- test/lib/index.js
+++ test/lib/index.js
@@ -10,72 +10,148 @@ describe('index', function() {
 
     describe('block', function() {
         describe('passes', function() {
-            it('simple', function() {
-                expect('input').to.be.a.block('input');
+            describe('positives', function() {
+                it('simple', function() {
+                    expect('input').to.be.a.block('input');
+                });
+
+                it('with mod', function() {
+                    expect('input_focused input').to.be.a.block('input');
+                });
             });
 
-            it('not first', function() {
-                expect('input_focused input').to.be.a.block('input');
+            describe('negatives', function() {
+                it('simple', function() {
+                    expect('button').to.not.be.a.block('input');
+                });
+
+                it('with mod', function() {
+                    expect('button_focused button').to.not.be.a.block('input');
+                });
             });
         });
 
         describe('throws error', function() {
-            it('simple', function() {
-                const assert = function() {
-                    expect('input_focused').to.be.a.block('input');
-                };
+            describe('positives', function() {
+                it('simple', function() {
+                    const assert = function() {
+                        expect('input_focused').to.be.a.block('input');
+                    };
 
-                expect(assert).to.throw(Error);
+                    expect(assert).to.throw(Error);
+                });
+
+                it('nonsense', function() {
+                    const assert = function() {
+                        expect('l;ks9d').to.be.a.block('input');
+                    };
+
+                    expect(assert).to.throw(Error);
+                });
             });
 
-            it('nonsense', function() {
-                const assert = function() {
-                    expect('l;ks9d').to.be.a.block('input');
-                };
+            describe('negatives', function() {
+                it('simple', function() {
+                    const assert = function() {
+                        expect('input').to.not.be.a.block('input');
+                    };
 
-                expect(assert).to.throw(Error);
+                    expect(assert).to.throw(Error);
+                });
+
+                it('with mod', function() {
+                    const assert = function() {
+                        expect('button_focused button').to.not.be.a.block('button');
+                    };
+
+                    expect(assert).to.throw(Error);
+                });
             });
         });
     });
 
     describe('elem', function() {
         describe('passes', function() {
-            it('simple', function() {
-                expect('input__control').to.be.an.elem({
-                    block: 'input',
-                    elem: 'control'
+            describe('positives', function() {
+                it('simple', function() {
+                    expect('input__control').to.be.an.elem({
+                        block: 'input',
+                        elem: 'control'
+                    });
+                });
+
+                it('within a mix', function() {
+                    expect('input sidebar__input').to.be.an.elem({
+                        block: 'sidebar',
+                        elem: 'input'
+                    });
                 });
             });
 
-            it('within a mix', function() {
-                expect('input sidebar__input').to.be.an.elem({
-                    block: 'sidebar',
-                    elem: 'input'
+            describe('negatives', function() {
+                it('simple', function() {
+                    expect('input__control').to.not.be.an.elem({
+                        block: 'input',
+                        elem: 'icon'
+                    });
+                });
+
+                it('within a mix', function() {
+                    expect('input sidebar__input').to.not.be.an.elem({
+                        block: 'content',
+                        elem: 'input'
+                    });
                 });
             });
         });
 
         describe('throws error', function() {
-            it('simple', function() {
-                const assert = function() {
-                    expect('input__icon').to.be.an.elem({
-                        block: 'input',
-                        elem: 'control'
-                    });
-                };
+            describe('positives', function() {
+                it('simple', function() {
+                    const assert = function() {
+                        expect('input__icon').to.be.an.elem({
+                            block: 'input',
+                            elem: 'control'
+                        });
+                    };
 
-                expect(assert).to.throw(Error);
+                    expect(assert).to.throw(Error);
+                });
+
+                it('nonsense', function() {
+                    const assert = function() {
+                        expect('sidebar_input asdfkj9').to.be.an.elem({
+                            block: 'sidebar',
+                            elem: 'input'
+                        });
+                    };
+
+                    expect(assert).to.throw(Error);
+                });
             });
 
-            it('nonsense', function() {
-                const assert = function() {
-                    expect('sidebar_input asdfkj9').to.be.an.elem({
-                        block: 'sidebar',
-                        elem: 'input'
-                    });
-                };
+            describe('negatives', function() {
+                it('simple', function() {
+                    const assert = function() {
+                        expect('input__icon').to.not.be.an.elem({
+                            block: 'input',
+                            elem: 'icon'
+                        });
+                    };
 
-                expect(assert).to.throw(Error);
+                    expect(assert).to.throw(Error);
+                });
+
+                it('with mod', function() {
+                    const assert = function() {
+                        expect('sidebar__input sidebar').to.not.be.an.elem({
+                            block: 'sidebar',
+                            elem: 'input'
+                        });
+                    };
+
+                    expect(assert).to.throw(Error);
+                });
             });
         });
     });
