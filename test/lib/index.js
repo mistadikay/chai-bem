5872b18a39766c138a811bc42f0e153e501df868
mistadikay <koltsov.d@gmail.com>
2015-08-30 17:37:40 +0700

    :space_invader: add missing tests for throw error in block and element

--- test/lib/index.js
+++ test/lib/index.js
@@ -9,36 +9,74 @@ describe('index', () => {
     });
 
     describe('block', () => {
-        it('simple', () => {
-            expect('input').to.be.a.block('input');
+        describe('passes', () => {
+            it('simple', () => {
+                expect('input').to.be.a.block('input');
+            });
+
+            it('not first', () => {
+                expect('input_focused input').to.be.a.block('input');
+            });
         });
 
-        it('not first', () => {
-            expect('input_focused input').to.be.a.block('input');
-        });
+        describe('throws error', () => {
+            it('simple', () => {
+                const assert = function() {
+                    expect('input_focused').to.be.a.block('input');
+                };
 
-        it.skip('TODO throw error tests', () => {
+                expect(assert).to.throw(Error);
+            });
 
+            it('nonsense', () => {
+                const assert = function() {
+                    expect('l;ks9d').to.be.a.block('input');
+                };
+
+                expect(assert).to.throw(Error);
+            });
         });
     });
 
     describe('elem', () => {
-        it('simple', () => {
-            expect('input input__control').to.be.an.elem({
-                block: 'input',
-                elem: 'control'
+        describe('passes', () => {
+            it('simple', () => {
+                expect('input input__control').to.be.an.elem({
+                    block: 'input',
+                    elem: 'control'
+                });
+            });
+
+            it('within a mix', () => {
+                expect('input input__control sidebar__input').to.be.an.elem({
+                    block: 'sidebar',
+                    elem: 'input'
+                });
             });
         });
 
-        it('within a mix', () => {
-            expect('input input__control sidebar__input').to.be.an.elem({
-                block: 'sidebar',
-                elem: 'input'
+        describe('throws error', () => {
+            it('simple', () => {
+                const assert = function() {
+                    expect('input input__icon').to.be.an.elem({
+                        block: 'input',
+                        elem: 'control'
+                    });
+                };
+
+                expect(assert).to.throw(Error);
             });
-        });
 
-        it.skip('TODO throw error tests', () => {
+            it('nonsense', () => {
+                const assert = function() {
+                    expect('sidebar_input asdfkj9').to.be.an.elem({
+                        block: 'sidebar',
+                        elem: 'input'
+                    });
+                };
 
+                expect(assert).to.throw(Error);
+            });
         });
     });
 });
