e270e8fcff14500da0dfa2d9df4c4841127c9158
mistadikay <koltsov.d@gmail.com>
2015-11-27 11:08:36 +0700

    :heavy_plus_sign: add validBlock property assertion

--- test/lib/block.js
+++ test/lib/block.js
@@ -7,36 +7,36 @@ describe('block', function() {
             describe('passes', function() {
                 describe('positives', function() {
                     it('simple', function() {
-                        expect(DOMElement('input')).to.be.a.block;
+                        expect(DOMElement('input')).to.be.a.validBlock;
                     });
 
                     it('with mod', function() {
-                        expect(DOMElement('input_focused input')).to.be.a.block;
+                        expect(DOMElement('input_focused input')).to.be.a.validBlock;
                     });
                 });
 
                 describe('negatives', function() {
                     it('element', function() {
-                        expect(DOMElement('button__icon_type_search')).to.not.be.a.block;
+                        expect(DOMElement('button__icon_type_search')).to.not.be.a.validBlock;
                     });
 
                     it('with mod', function() {
-                        expect(DOMElement('button_big')).to.not.be.a.block;
+                        expect(DOMElement('button_big')).to.not.be.a.validBlock;
                     });
                 });
             });
 
-            describe.skip('throws error', function() {
+            describe('throws error', function() {
                 describe('positives', function() {
                     it('simple', function() {
                         expect(function() {
-                            expect(DOMElement('input_focused')).to.be.a.block;
+                            expect(DOMElement('input_focused')).to.be.a.validBlock;
                         }).to.throw(Error);
                     });
 
                     it('nonsense', function() {
                         expect(function() {
-                            expect(DOMElement('l;ks9d')).to.be.a.block;
+                            expect(DOMElement('l;ks9d')).to.be.a.validBlock;
                         }).to.throw(Error);
                     });
                 });
@@ -44,13 +44,13 @@ describe('block', function() {
                 describe('negatives', function() {
                     it('simple', function() {
                         expect(function() {
-                            expect(DOMElement('input')).to.not.be.a.block;
+                            expect(DOMElement('input')).to.not.be.a.validBlock;
                         }).to.throw(Error);
                     });
 
                     it('with mod', function() {
                         expect(function() {
-                            expect(DOMElement('button_focused button')).to.not.be.a.block;
+                            expect(DOMElement('button_focused button')).to.not.be.a.validBlock;
                         }).to.throw(Error);
                     });
                 });
