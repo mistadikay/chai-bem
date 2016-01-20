7835c345f01c55da095b229241182cf6b3794224
mistadikay <koltsov.d@gmail.com>
2016-01-20 12:34:41 +0700

    :heavy_check_mark: fix incorrect bem negative assertion

--- test/lib/bemjson.js
+++ test/lib/bemjson.js
@@ -3,112 +3,225 @@ import { runTests } from '../helpers';
 
 describe('bemjson', function() {
     runTests(function(DOMElement) {
-        it('block', function() {
-            expect(DOMElement(
-                'input'
-            )).to.be.bem({
-                block: 'input'
+        describe('positives', function() {
+            it('block', function() {
+                expect(DOMElement(
+                    'input'
+                )).to.be.bem({
+                    block: 'input'
+                });
             });
-        });
 
-        it('elem', function() {
-            expect(DOMElement(
-                'input__control'
-            )).to.be.bem({
-                block: 'input',
-                elem: 'control'
+            it('elem', function() {
+                expect(DOMElement(
+                    'input__control'
+                )).to.be.bem({
+                    block: 'input',
+                    elem: 'control'
+                });
             });
-        });
 
-        it('block single mod', function() {
-            expect(DOMElement(
-                'input input_focused'
-            )).to.be.bem({
-                block: 'input',
-                mods: {
-                    focused: true
-                }
-            });
-        });
-
-        it('block multi mods', function() {
-            expect(DOMElement(
-                'input input_focused input_color_black'
-            )).to.be.bem({
-                block: 'input',
-                mods: {
-                    focused: true,
-                    color: 'black'
-                }
-            });
-        });
-
-        it('elem single mod', function() {
-            expect(DOMElement(
-                'input__control input__control_focused'
-            )).to.be.bem({
-                block: 'input',
-                elem: 'control',
-                mods: {
-                    focused: true
-                }
-            });
-        });
-
-        it('elem multi mods', function() {
-            expect(DOMElement(
-                'input__control input__control_focused input__control_color_red'
-            )).to.be.bem({
-                block: 'input',
-                elem: 'control',
-                mods: {
-                    focused: true,
-                    color: 'red'
-                }
-            });
-        });
-
-        it('mix', function() {
-            expect(DOMElement(
-                'input stuff__yo'
-            )).to.be.bem({
-                block: 'input',
-                mix: {
-                    block: 'stuff',
-                    elem: 'yo'
-                }
-            });
-        });
-
-        it('mix with mods', function() {
-            expect(DOMElement(
-                'input stuff__yo stuff__yo_focused'
-            )).to.be.bem({
-                block: 'input',
-                mix: {
-                    block: 'stuff',
-                    elem: 'yo',
+            it('block single mod', function() {
+                expect(DOMElement(
+                    'input input_focused'
+                )).to.be.bem({
+                    block: 'input',
                     mods: {
                         focused: true
                     }
-                }
+                });
             });
-        });
 
-        it('multi mix', function() {
-            expect(DOMElement(
-                'input random stuff__yo'
-            )).to.be.bem({
-                block: 'input',
-                mix: [
-                    {
-                        block: 'random'
-                    },
-                    {
+            it('block multi mods', function() {
+                expect(DOMElement(
+                    'input input_focused input_color_black'
+                )).to.be.bem({
+                    block: 'input',
+                    mods: {
+                        focused: true,
+                        color: 'black'
+                    }
+                });
+            });
+
+            it('elem single mod', function() {
+                expect(DOMElement(
+                    'input__control input__control_focused'
+                )).to.be.bem({
+                    block: 'input',
+                    elem: 'control',
+                    mods: {
+                        focused: true
+                    }
+                });
+            });
+
+            it('elem multi mods', function() {
+                expect(DOMElement(
+                    'input__control input__control_focused input__control_color_red'
+                )).to.be.bem({
+                    block: 'input',
+                    elem: 'control',
+                    mods: {
+                        focused: true,
+                        color: 'red'
+                    }
+                });
+            });
+
+            it('mix', function() {
+                expect(DOMElement(
+                    'input stuff__yo'
+                )).to.be.bem({
+                    block: 'input',
+                    mix: {
                         block: 'stuff',
                         elem: 'yo'
                     }
-                ]
+                });
+            });
+
+            it('mix with mods', function() {
+                expect(DOMElement(
+                    'input stuff__yo stuff__yo_focused'
+                )).to.be.bem({
+                    block: 'input',
+                    mix: {
+                        block: 'stuff',
+                        elem: 'yo',
+                        mods: {
+                            focused: true
+                        }
+                    }
+                });
+            });
+
+            it('multi mix', function() {
+                expect(DOMElement(
+                    'input random stuff__yo'
+                )).to.be.bem({
+                    block: 'input',
+                    mix: [
+                        {
+                            block: 'random'
+                        },
+                        {
+                            block: 'stuff',
+                            elem: 'yo'
+                        }
+                    ]
+                });
+            });
+        });
+
+        describe('negatives', function() {
+            it('block', function() {
+                expect(DOMElement(
+                    'input__control'
+                )).to.not.be.bem({
+                    block: 'input'
+                });
+            });
+
+            it('elem', function() {
+                expect(DOMElement(
+                    'input'
+                )).to.not.be.bem({
+                    block: 'input',
+                    elem: 'control'
+                });
+            });
+
+            it('block single mod', function() {
+                expect(DOMElement(
+                    'input input_hovered'
+                )).to.not.be.bem({
+                    block: 'input',
+                    mods: {
+                        focused: true
+                    }
+                });
+            });
+
+            it('block multi mods', function() {
+                expect(DOMElement(
+                    'input input_hovered input_color_black'
+                )).to.not.be.bem({
+                    block: 'input',
+                    mods: {
+                        focused: true,
+                        color: 'black'
+                    }
+                });
+            });
+
+            it('elem single mod', function() {
+                expect(DOMElement(
+                    'input__control input__control_hovered'
+                )).to.not.be.bem({
+                    block: 'input',
+                    elem: 'control',
+                    mods: {
+                        focused: true
+                    }
+                });
+            });
+
+            it('elem multi mods', function() {
+                expect(DOMElement(
+                    'input__control input__control_focused input__control_color_white'
+                )).to.not.be.bem({
+                    block: 'input',
+                    elem: 'control',
+                    mods: {
+                        focused: true,
+                        color: 'red'
+                    }
+                });
+            });
+
+            it('mix', function() {
+                expect(DOMElement(
+                    'input stuff__no'
+                )).to.not.be.bem({
+                    block: 'input',
+                    mix: {
+                        block: 'stuff',
+                        elem: 'yo'
+                    }
+                });
+            });
+
+            it('mix with mods', function() {
+                expect(DOMElement(
+                    'input stuff__yo stuff__yo_hovered'
+                )).to.not.be.bem({
+                    block: 'input',
+                    mix: {
+                        block: 'stuff',
+                        elem: 'yo',
+                        mods: {
+                            focused: true
+                        }
+                    }
+                });
+            });
+
+            it('multi mix', function() {
+                expect(DOMElement(
+                    'input random stuff__no'
+                )).to.not.be.bem({
+                    block: 'input',
+                    mix: [
+                        {
+                            block: 'random'
+                        },
+                        {
+                            block: 'stuff',
+                            elem: 'yo'
+                        }
+                    ]
+                });
             });
         });
 
